b5e03e54f79f9cf63cb681499dfbeaaf042c4e93
Tur-ph <67241029+Tur-ph@users.noreply.github.com>
2020-06-21 23:18:00 +0100

    Created Index.ks

---
+++ index.js
@@ -0,0 +1,3 @@
+// Hello there! This will be a tutorial with somewhat detailed description of what is happening...
+
+const discord = require('discord.js'); // here you are requiring the discord.js package
