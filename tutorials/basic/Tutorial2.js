9b88c2b3a9a2d02ef1c4ad20ac951fd2b054996e
Tur-ph <67241029+Tur-ph@users.noreply.github.com>
2020-06-22 17:05:31 +0100

    Update Tutorial2.js

--- tutorials/basic/Tutorial2.js
+++ tutorials/basic/Tutorial2.js
@@ -1,1 +1,11 @@
-..
+const discord = require('discord.js');
+const client = new discord.Client();
+
+client.login(BOT TOKEN);
+
+client.on('message', message => {
+  if(message.content.startsWith('!ping') {
+    message.reply('Pong!');
+});
+
+// If you do not understand what any of this means please go back to tutorial 1 and read it over without skipping any details!
