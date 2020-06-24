69bf5aa6d8af1a75b9d6d67a383d87d4ac28852b
Tur-ph <67241029+Tur-ph@users.noreply.github.com>
2020-06-24 20:33:53 +0100

    Tutorial 2 was Updated
    I have almost finished it but right now I cannot be bothered!

--- tutorials/basic/Tutorial2.js
+++ tutorials/basic/Tutorial2.js
@@ -9,3 +9,43 @@ client.on('message', message => {
 });
 
 // If you do not understand what any of this means please go back to tutorial 1 and read it over without skipping any details!
+
+// In this tutorial we will be adding arguments into the commands! This is extremely useful for Moderation Commands!
+
+// First we need to make a message event! To do so we can do this:
+
+client.on('message', message => {
+  // Now let's add a "args" variable
+  const args =
+  // We now need to get the contents of the message, we can do this by writing "message.content"
+  const args = message.content
+  
+  // Now we slice the prefix off the command! To do so we can simply make a prefix variable!
+  const prefix = '!';
+  const args = message.content.slice(prefix.length)
+  
+  // Now, we need to split it all up by a space, doing so will allow us to calculate the amount of args
+  const prefix = '!';
+  const args = message.content.slice(prefix.length).split(' ');
+  
+  // Now, that is the main body of the arguments done! If you want you can skip this next part, but I recommend you do not. It's for shorten commands
+  
+  // Now we have the arguments variable defined, we can now start making the commands or "cmds" variable!
+  // To start off, we need to move all the arguments over by 1, we can do this by writing:
+  const cmds = args.shift()
+  
+  // Now we can quickly make sure the cmds variable is in lower case! This is an easy function to do:
+  const cmds = args.shift().toLowerCase();
+  
+  // Now that is done, we can move onto th different types of "if" statements you can use:
+  
+    // First option: SWITCH
+      // I do not use switch personally but it's up to you what you decide, you'd set it up like:
+      switch(cmds) {
+        case(ping):
+          message.reply('Pong!')
+          break;
+      }
+  
+      // To start of with, the switch function stores a variable. The "case" statements check the variable
+});
